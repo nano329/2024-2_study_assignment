2af653d4c6b0b48292fffdab1845a9de8669c68a
nano329 <nano329@naver.com>
2024-10-31 14:36:44 +0900

    asdf

--- assignment4/4_2_star_ans.cs
+++ assignment4/4_2_star_ans.cs
@@ -11,6 +11,51 @@ namespace star
             int size = 2 * (radius + 1);
 
             // ---------- TODO ----------
+            int interval1 = size / 3, interval2 = interval1 - ((size % 3) == 0 ? 1 : 0);
+            
+            for(int j = 0; j < radius * 2 + 1 + interval1; j++){
+                Console.Write(" ");
+            }
+            Console.Write("*");
+            for(int j = 0; j < interval2; j++){
+                Console.Write(" ");
+            }
+            Console.WriteLine("*");
+            //Console.WriteLine(' ' * (radius * 2 + 1 + interval1) + '*' + ' ' * interval2 + '*');
+            for(int i = 1; i < size; i++){
+                if(i == 1 || i == size - 1){
+                    Console.Write(" ");
+                    for(int j = 0; j < radius * 2 - 1; j++){
+                        Console.Write("*");
+                    }
+                    Console.Write(" ");
+                }
+                else{
+                    Console.Write("*");
+                    for(int j = 0; j < radius * 2; j++){
+                        Console.Write(" ");
+                    }
+                }
+                if(i == interval1 || i == interval1 + interval2 + 1){
+                    for(int j = 0; j < size; j++){
+                        Console.Write("*");
+                    }
+                }
+                else{
+                    for(int j = 0; j < interval1; j++){
+                        Console.Write(" ");
+                    }
+                    Console.Write("*");
+                    for(int j = 0; j < interval2; j++){
+                        Console.Write(" ");
+                    }
+                    Console.Write("*");
+                    for(int j = 0; j < size - interval1 - interval2 - 2; j++){
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
 
 
             // --------------------
